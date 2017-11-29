fafd2d452a5973e99ec7b664f8929696f47590ca
OSLU <oswaldo.luna@alumnos.udg.mx>
2017-11-29 15:45:24 -0600

    se modifico para que se vea 5 decimales solamente se arreglo error en captura

--- app/src/main/java/com/numericsystool/flycat/numericsys/regla_Falsa.java
+++ app/src/main/java/com/numericsystool/flycat/numericsys/regla_Falsa.java
@@ -1,5 +1,6 @@
 package com.numericsystool.flycat.numericsys;
 import java.util.Scanner;
+import java.text.DecimalFormat;
 /*
  * [a,b] son intervalos
  * f: funcion
@@ -10,49 +11,48 @@ import java.util.Scanner;
  */
 
 public class regla_Falsa {
-    public static double f(double x){//este se modifcara segun el ejemplo de funtion
-        return -(x*x)-(2*x)+1;
+    /*definicion de una funcion de variable*/
+    public static double f(double x){
+        return -(x*x)-(2*x)+1;// se cambiara por funciones de ejemplo
     }
+
     public static void funcion(String[] args) {
         Scanner a1 = new Scanner(System.in);
         Scanner b1 = new Scanner(System.in);
 
         System.out.println("Intervalo inferior:");
-        a1.nextDouble();
-        System.out.println("Intervalo superior");
-        b1.nextDouble();
         double a=a1.nextDouble();
+        System.out.println("Intervalo superior");
         double b=b1.nextDouble();
+        // formato de 5 decimales
+        DecimalFormat forma = new DecimalFormat("##.#####");
         double  c;
-        int it_limite=10;
         int it=0;
         /*[a,b] es el intervalo F(a)*f(b)<0 */
         if(f(a)*f(b)>0)
         {
             System.out.println("No cumple. trate otro intevalo. ");
         }
-        while(1>0){
-            //iteración
-            it=it++;
-            c=((f(b)*a)-(f(a)*b))/(f(b)-f(a));
-            System.out.println(+it+"  "+ a+ "  "+ c );
-            System.out.println(+b+"  "+f(a)+"  "+f(c));
-            if(it>=it_limite)
-            {
-                System.out.println("se excedio el limite de iteraciones.");
-                break;
-            }
-            if(f(c)<=1e-10){
-                System.out.println("La raiz aproximada es r = "+ c);
-                System.out.println("El valor de f en r = " + f(c));
-                return;
-            }
-            if(f(a)*f(c)<0)
-            {
-                b=c;
-            }
-            else{
-                a=c;
+        else{
+            while(1>0){
+                //iteración
+                it=it+1;
+                c=a-(f(a)*(a-b))/(f(a)-f(b));//funcion de regla falsa
+                System.out.println(+it+"  "+ forma.format(a)+ "  "+ forma.format(c) );
+                System.out.println(+b+"  "+forma.format(f(a))+"  "+forma.format(f(c)));
+
+                if(f(c)<=1e-5){
+                    System.out.println("La raiz aproximada es r = "+ forma.format(c));
+                    System.out.println("El valor de f en r = " + forma.format(f(c)));
+                    return;
+                }
+                if(f(a)*f(c)<0)
+                {
+                    b=c;
+                }
+                else{
+                    a=c;
+                }
             }
         }
     }
